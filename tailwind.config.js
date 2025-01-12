d3d1afb0f5566c22317a7295de921b4163f81d96
Foad <fwad40901@gmail.com>
2025-01-12 09:37:20 +0330

    clear files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   content: ['*.{html,js}'],
   theme: {
     extend: {
-      fontFamily: {
-        sofia400: ['sofia400'],
-        sofia500: ['sofia500'],
-        sofia600: ['sofia600'],
-        sofia700: ['sofia700'],
-      },
+      fontFamily: {},
     },
   },
   plugins: [],
